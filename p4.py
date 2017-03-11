8fd3a53fd9fb79ccf64c95a917f52718b69d1b66
brian wiltse <wiltseb@oregonstate.edu>
2017-03-11 06:42:22 -0800

    Prim's works!!!

--- p4.py
+++ p4.py
@@ -15,6 +15,7 @@ Returns a list of lists in the format:
 [identifyer, x-coordinate, y-coordinate]
 '''
 def getInputData(filename):
+
     data = []
     with open(filename) as inFile:
         for line in inFile:
@@ -29,6 +30,7 @@ Gets distance between ordered pairs (x1,y1) and (x2,y2) rounded to the
 nearest integer.
 '''
 def getDistance(x1,y1,x2,y2):
+
     x = pow((x1-x2), 2)
     y = pow((y1-y2), 2)
     return int(math.sqrt(x+y))
@@ -66,8 +68,8 @@ G = {
 
 So, G[cityA][cityB} returns the distance between city A and city B
 
-''' 
-def buildConnectedGraph(vertices):
+'''
+def buildConnectedGraph(vertices): 
     G = {}
     #put each vertex in adjacency list for every other vertex
     for i in range(len(vertices)):
@@ -76,51 +78,77 @@ def buildConnectedGraph(vertices):
         city1y = vertices[city1Identifyer][2]
         G[city1Identifyer] = {}
         for j in range(len(vertices)):
-            if i != j:
+            if i != j:  #don't put city in its own adjacency list
                 city2Identifyer = vertices[j][0]
                 city2x = vertices[city2Identifyer][1]
-                city2y = vertices[city2Identifyer][2]        
+                city2y = vertices[city2Identifyer][2]
+                #G[u][v] is the distance between u and v
                 G[city1Identifyer][city2Identifyer] = getDistance(city1x, city1y, city2x, city2y)
     return G
 
-def sortEdges(adjacentVertices):
-    '''
-    sortedAdjList = []
-    sortedGraph = {}
-    for i in range(len(G)):
-        currVertex = G[i]
-        sortedAdjList = sorted(currVertex.items(), key=operator.itemgetter(1))
-        sortedGraph[i] = sortedAdjList
-    '''
-    #https://www.ics.uci.edu/~eppstein/PADS/MinimumSpanningTree.py
-    sortedGraph = sorted((G[u][v],u,v) for u in G for v in G[u])
-    return sortedGraph
-    
 
-
-
-def primms_mst(G, startCityID):    
+'''
+Takes in a graph, G (a dictionary of dictionaries), and returns a new graph with keys "predecessor" and "distanceTo",
+which indicate the current vertex's parent vertex and how far the current vertex and parent are away from each other, respectively
+'''
+def prims_mst(G, startCityID):
+    #initialize each vertex in G with an infinite distanceTo
+    keys = G.keys()
     for u in G:
         G[u]['distanceTo'] = float('inf')
-    Gcopy = G.copy()
+    newG = {}
     currCityID = startCityID
-    parent = {}
-    while Gcopy:   
-        u = Gcopy.pop(currCityID, None)
+    #Start city's predecessor is None, the distanceTo is 0
+    G[currCityID]['predecessor'] = None;
+    G[currCityID]['distanceTo'] = 0;
+    #Take the city(and its adjacency list) with the closest distance out of original graph and put it in a new
+    #graph with predecessor and distanceTo data
+    while G:
+        u = G.pop(currCityID, None)
+        newG[currCityID] = u
         currMin = float('inf')
-        minVID = -1
         for v in u:
-            if u[v] < currMin and v != 'distanceTo':
-                currMin = u[v]
-                minVID = v
-                G[v]['distanceTo'] = u[v]
-        parent[v] = currCityID
-        currCityID = minVID
-        print currCityID
+            #Only interested in vertices still in G ('popped' vertices have already been visited)
+            if v in G and v != 'predecessor' and v != 'distanceTo':
+                if u[v] < G[v]['distanceTo'] :
+                    G[v]['distanceTo'] = u[v]
+                    G[v]['predecessor'] = currCityID
+        minDist = float('inf')
+        for i in keys:
+            if i != 'predecessor' and i != 'distanceTo' and i in G:
+                if G[i]['distanceTo'] < minDist:
+                    minDist = G[i]['distanceTo']
+                    currCityID = i
         
-                
-            
-    return parent
+    return newG
+
+'''
+Takes in a graph that has MST data and prints MST heirarchy
+Also prints number of cities for a sanity check
+'''
+def printMSTHeirarchy(MSTGraph):
+    cityCount = 0
+    for i in MSTGraph:
+        print str(i) + "'s predecessor is: " + str(MSTGraph[i]['predecessor']) + ". Distance is: " + str(MSTGraph[i]['distanceTo'])
+        cityCount += 1
+    print "Number of cities: " + str(cityCount)
+    totalEdgeWeights=0
+    for i in MSTGraph:
+        totalEdgeWeights += MSTGraph[i]['distanceTo']
+    print "Total edge weights: " + str(totalEdgeWeights)
+
+
+'''
+Verified Prim's works with the visualization from class at:
+http://rosulek.github.io/vamonos/demos/prims.html
+'''
+def testPrims():
+    G = { 'a':{'b':6,'c':3,'e':9},'b':{'a':6,'c':4,'d':2,'h':5,'i':4},'c':{'a':3,'b':4,'d':2,'e':9},
+         'd':{'b':2,'c':2,'f':8},'e':{'a':9,'c':9,'f':8,'g':18},'f':{'e':8,'d':8,'h':9,'g':10},
+         'g':{'i':4,'h':3,'f':10,'e':18},'h':{'f':9,'g':3,'b':5},'i':{'b':4,'g':4}}
+    printMSTHeirarchy(prims_mst(G, 'b'))
+
+
 
 '''
 FUNCTION USAGE IN END PRODUCT(Must accept problem instances on command line):
@@ -130,13 +158,16 @@ createOutputFile(tourLength, tour, sys.argv[2])
 '''
 
 
-
+'''
 vertices = getInputData("tsp_example_1.txt");
 G = buildConnectedGraph(vertices)
-primms_mst(G, 0)
+newG = prims_mst(G, 75)
+printMSTHeirarchy(newG)
+
+'''
 
 
-
+testPrims()
 
 
 
