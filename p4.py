1b2a71621dde42c627be7ec5cfbd8b74b4d87a85
Greg Fernandez <fernangr@oregonstate.edu>
2017-03-17 19:33:40 -0400

    Added usage print on IndexError with argv[1]

--- p4.py
+++ p4.py
@@ -112,7 +112,7 @@ nearest integer.
 def getDistance(x1,y1,x2,y2):
     dx = x2-x1
     dy = y2 - y1
-    
+
     return int(round(math.sqrt(dx*dx + dy*dy)))
 
 '''
@@ -470,7 +470,11 @@ def christofidesTSP(cities, inputFilename):
 
 totalTime = 0
 start = time.clock()
-inputFilename = sys.argv[1]
+try:
+    inputFilename = sys.argv[1]
+except IndexError:
+    print "usage: python p4.py inputFileName"
+    exit(1)
 cities = getInputData(inputFilename)
 
 #For large data sets, use nearest neighbor
@@ -488,4 +492,3 @@ else:
 
 totalTime = time.clock() - start
 print "Time: " + str(totalTime) + " sec"
-
